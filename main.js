bb9759b4a543a085a0dd3012d8b5466988eae5fd
Axeldias <106696074+Axeldias@users.noreply.github.com>
2023-09-04 10:49:27 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+
+function tocaSomclap  () {
+    document.querySelector('#som_tecla_Clap').play();
+
+
+}
+
+
+document.querySelector('.tecla_Clap').onclick = tocaSomClap;
+
+function tocaSomTim () {
+
+    
+}
+
