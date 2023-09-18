fa287d31047d4f5cee2da30af3c2d7d61e79d328
Axeldias <106696074+Axeldias@users.noreply.github.com>
2023-09-18 10:54:41 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,21 +1,24 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play()
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll(".tecla")
 
 
-function tocaSomclap  () {
-    document.querySelector('#som_tecla_Clap').play();
 
-
-}
-
-
-document.querySelector('.tecla_Clap').onclick = tocaSomClap;
-
-function tocaSomTim () {
-
+for ( let contador = 0; contador < listaDeTeclas.length; contador++ ){
     
-}
+    const tecla = listaDeTeclas[contador]
+    
+    const instrumento = tecla.classList[1];
 
+    const idAudio = `#som_${instrumento}`
+
+    listaDeTeclas[contador].onclick = function () {
+        tocaSom(idAudio)
+    }
+
+
+
+    console.log(contador);
+}
